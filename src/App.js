97e35615e61a70c3ce3481d9c07a6ac0def5db3d
Adenhall <jamesadenhall@gmail.com>
2020-05-28 16:34:29 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import MovieCard from './components/MovieCards.js'
 
-const API_KEY = process.env.REACT_APP_APIKEY;
+const API_KEY = "e32f02b7a91bd590c9e7305b54bba6de";
 
 export default function App() {
 
+  const [movieList, setMovieList] = useState(null);
   const getMovieList = async() => {
     let url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1`
 
     let data = await fetch(url);
     let result = await data.json();
     console.log("My list", result);
-
+    setMovieList(result.results)
   }
 
-  useEffect( {
+  useEffect(() => {
     getMovieList();
   }
-)
+  ,[])
+
+  if (movieList == null) {
+    return(<div>Loading</div>)
+  }
 
   return (
-    <div>
-      
+    <div className="d-flex justify-content-around movie-list">
+      {movieList.map(item => {
+        return <MovieCard movie = {item}/>
+      })}
     </div>
-  )
+  );
 }
---
+++ src/components/MovieCards.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Card, ListGroup, ListGroupItem} from 'react-bootstrap';
+
+export default function MovieCards(props) {
+    return (
+        <div>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${props.movie.poster_path}`}/>
+        <Card.Body>
+          <Card.Title>{props.movie.title}</Card.Title>
+          <Card.Text>
+            {props.movie.overview}
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>{props.movie.popularity} are watching!</ListGroupItem>
+          <ListGroupItem>{props.movie.adult ? 'Wooow hang on kids! 18+ Only' : 'Safe for below 18 years old'}</ListGroupItem>
+          <ListGroupItem>Release date: {props.movie.release_date}</ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+        </div>
+    )
+}
