951f361c60ca07fc1c5d250d8c7d10d726e0b94e
Adenhall <jamesadenhall@gmail.com>
2020-05-28 15:07:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const API_KEY = process.env.REACT_APP_APIKEY;
+
+export default function App() {
+
+  const getMovieList = async() => {
+    let url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1`
+
+    let data = await fetch(url);
+    let result = await data.json();
+    console.log("My list", result);
+
+  }
+
+  useEffect( {
+    getMovieList();
+  }
+)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
-  );
+  )
 }
-
-export default App;
