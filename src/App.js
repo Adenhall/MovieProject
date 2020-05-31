70b5be73b7e3a942c2ee202118fd13c5dbd3eb88
Adenhall <jamesadenhall@gmail.com>
2020-06-01 00:22:14 +0700

    Added learn more

--- src/App.js
+++ src/App.js
@@ -40,7 +40,49 @@ export default function App() {
     let url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`;
     let data = await fetch(url);
     let result = await data.json();
-    document.getElementsByClassName('movie-list').innerHTML = null;
+    document.getElementById('main-body').innerHTML = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">MOVIE 'N CHILL</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="${result.homepage}">See more</a>
+        </li>
+      </ul>
+    </div>
+    </nav>
+    <section>
+        <div class="container">
+          <div class="row">
+            <div class="col" style="color: white;">
+              <h2>${result.original_title}</h2>
+              <h3> Available on Blu-ray</h3>
+            </div>
+            <div class="col"></div>
+            <div class="w-100"></div>
+            <div class="col"><img src=${`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${result.poster_path}`} style="width: 400px;"/></div>
+            <div style="color: white;">
+              <h3>${result.overview}</h3>
+              <h3><i class="fas fa-angle-double-right"></i> Genres </br>
+              <h5>${result.genres.map(f => {return f.name}).join(', ')}</h5>
+              </h3>
+              <h3><i class="fas fa-angle-double-right"></i> Runtime</br>
+              <h5>${result.runtime} mins</h5>
+              </h3>
+              <h3><i class="fas fa-angle-double-right"></i> Produced by</br>
+              <h5>${result.production_companies.map(x => {return x.name}).join(', ')}</h5>
+              </h3>
+              <h3><i class="fas fa-angle-double-right"></i> More details <a href=${result.homepage}>here!</a>
+              </h3>
+            </div>
+          </div>
+          </div>
+      </section>`;
     console.log(result)
 
   }
@@ -68,9 +110,9 @@ export default function App() {
 
 
   return (
-    <div className="d-flex flex-column">
+    <div id="main-body" className="d-flex flex-column">
       <NavBar/>
-      <div className="d-flex justify-content-around movie-list">
+      <div id="movies" className="d-flex justify-content-around movie-list">
         {movieList.map(item => {
           return <MovieCard movie = {item} getMovieDetails = {getMovieDetails}/>
         })}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,9 @@ export default function NavBar() {
     return (
         <div>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home">MOVIE 'N CHILL</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
                 </Nav>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
